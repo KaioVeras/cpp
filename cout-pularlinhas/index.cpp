da8ca62e023973745fb167d8c08d6a26f416b7fd
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-08-10 16:21:59 -0300

    Create index.cpp

---
+++ cout-pularlinhas/index.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Código 01 - cout e pular linhas
+
+int main() {
+    cout << "1 série C\n";
+    cout << "Aula de C++ " << endl;
+    cout << "Prof. Fred";
+    return 0;
+}
