5d4c6fa9edf9f7d1a971538db14dc3f43e25f7f9
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-08-13 18:04:13 -0300

    Create index.cpp

---
+++ variaveis/index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int carlos = 15; //numeros inteiros
+    double luis = 1.70; //numeros decimais
+    char cleber = 'A'; //carcter unico
+    string alex = "Kaio Vítor"; //texto
+    bool sinara = true; //true ou false
+
+    cout << "Meu nome é " << alex << ", tenho " << carlos << " anos e meço " << luis << " m.";
+
+    return 0;
+}
