44a34124c13a3a5458832a418e0a80950102ca61
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-08-13 18:04:03 -0300

    Create index.cpp

---
+++ entrada-de-dados/index.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Me informe um valor para x: ";
+    cin >> x;
+    cout << "O valor de x é " << x;
+
+    return 0;
+}
