baf34ac2ba31f8326f447a350a5df97cb5d94a25
Kaio Vítor Pinheiro Veras <kaiooveras@gmail.com>
2022-08-13 18:04:01 -0300

    Create index.cpp

---
+++ calculadora-simples/index.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, s;
+
+    cout << "Me informe um número para a soma: ";
+    cin >> a;
+
+    cout << "Me informe outro número para a soma: ";
+    cin >> b;
+    s = a + b;
+
+    cout << "A soma dos números informados: " << s;
+    return 0;
+}
